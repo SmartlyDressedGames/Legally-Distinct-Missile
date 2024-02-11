2f856b8f2ed59779e5d86c552d1cba50eae0904a
sunnamed434 <cetnikletsplay@gmail.com>
2024-02-11 23:25:40 +0200

    Make string as interpolated in CommandRocket

--- Rocket.Unturned/Commands/CommandRocket.cs
+++ Rocket.Unturned/Commands/CommandRocket.cs
@@ -50,7 +50,7 @@ namespace Rocket.Unturned.Commands
         {
             if (command.Length == 0)
             {
-                UnturnedChat.Say(caller, "RocketModFix v" + Assembly.GetExecutingAssembly().GetName().Version + " for Unturned v" + Provider.APP_VERSION);
+                UnturnedChat.Say(caller, $"RocketModFix v{Assembly.GetExecutingAssembly().GetName().Version} for Unturned v{Provider.APP_VERSION}");
                 UnturnedChat.Say(caller, "https://github.com/RocketModFix/RocketModFix/");
                 UnturnedChat.Say(caller, "https://discord.gg/z6VM7taWeG");
                 return;
